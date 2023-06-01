2a6d62cadb9aa46f462199e44ad5ff0e60b08e2b
cja <ywf151sl@duck.com>
2023-06-01 19:48:43 +1000

    commit

--- components/form.tsx
+++ components/form.tsx
@@ -23,7 +23,8 @@ export default function Form({ type }: { type: "login" | "register" }) {
             password: e.currentTarget.password.value,
             // @ts-ignore
           }).then(({ error }) => {
-            if (error) {
+console.log('Sign In response:', error);           
+ if (error) {
               setLoading(false);
               toast.error(error);
             } else {
--- middleware.ts
+++ middleware.ts
@@ -2,22 +2,21 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
-  // Get the pathname of the request (e.g. /, /protected)
   const path = req.nextUrl.pathname;
-
-  // If it's the root path, just render it
-  if (path === "/") {
-    return NextResponse.next();
-  }
 
   const session = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET,
   });
 
+  // Add session logging
+  console.log("Middleware session:", session);
+
   if (!session && path === "/protected") {
+    console.log("Middleware Redirect: /protected -> /login");
     return NextResponse.redirect(new URL("/login", req.url));
   } else if (session && (path === "/login" || path === "/register")) {
+    console.log("Middleware Redirect: /login or /register -> /protected");
     return NextResponse.redirect(new URL("/protected", req.url));
   }
   return NextResponse.next();
